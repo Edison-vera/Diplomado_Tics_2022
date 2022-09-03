dcdcc17448d20d6d19517942e53f1882537c80ea
Edison-vera <edisonefvc15@gmail.com>
2022-09-02 20:13:47 -0500

    Js end point tablas

---
+++ Ciclo 3/Introduccion-js/index.js
@@ -0,0 +1,11 @@
+let nombre = "Andres";
+let apellido = "Vera";
+console.log(`Nombre: ${nombre} Apellido: ${apellido}`);
+
+
+//Referenciar div
+let div = document.getElementById('app');
+//Adicionar texto
+div.innerText = 'Hola mundo desde js';
+//Adicionar codigo desde HTML
+div.innerHTML = '<h2>Hola mundo desde js con innerHtml</h2>';
