96aca99d5d03262ccb528889debd3414bd87711a
Edison-vera <edisonefvc15@gmail.com>
2022-10-21 19:30:50 -0500

    Se agrego git ignore

--- Ciclo 4/Proyecto Backend/src/app.js
+++ Ciclo 4/Proyecto Backend/src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 //Setting
-app.set('port', process.env.PORT || 5005);
+app.set('port', process.env.PORT || 5000);
 
 /* This is exporting the app instance of the express module. */
 module.exports = app;
--- Ciclo 4/Proyecto Backend/src/index.js
+++ Ciclo 4/Proyecto Backend/src/index.js
@@ -4,6 +4,43 @@ require('dotenv').config();
 /* Importing the app.js file. */
 const app = require('./app');
 
+app.get('/', (req, res) => {
+    res.send('Hola mi server esta en express!!!');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+    res.send('Esto es una nueva ruta!!!');
+});
+
+app.get('/categorias', (req, res) => {
+    res.json([{
+            id: 1,
+            name: 'Categoria 1'
+        },
+        {
+            id: 2,
+            name: 'Categoria 2'
+        },
+        {
+            id: 3,
+            name: 'Categoria 3'
+        },
+        {
+            id: 4,
+            name: 'Categoria 4'
+        }
+    ]);
+});
+
+app.get('/categorias/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+        id,
+        id: 3,
+        name: 'Categoria 3'
+    })
+})
+
 /**
  * The main function is an asynchronous function that waits for the app to listen on the port specified
  * by the app.get('port') function, then logs a message to the console.
